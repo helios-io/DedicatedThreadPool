c04e33d7749ba4c63a6d0862ebd0494f4fd73005
huysentruitw <wouter_huysentruit@hotmail.com>
2020-09-03 17:03:59 +0200

    Target netstandard2.0 + update projects to new csproj layout (#19)
    * Target netstandard2.0 + update projects to new csproj layout
    
    * Do not generate documation file

---
+++ src/core/Helios.DedicatedThreadPool/_Visibility.cs
@@ -0,0 +1,6 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Helios.DedicatedThreadPool.Tests")]
+[assembly: InternalsVisibleTo("Helios.DedicatedThreadPool.Tests.Performance")]
+[assembly: InternalsVisibleTo("Helios.DedicatedThreadPool.VsThreadpoolBenchmark")]
+[assembly: InternalsVisibleTo("Helios.DedicatedThreadPool.VsDedicatedThreadFiber")]
