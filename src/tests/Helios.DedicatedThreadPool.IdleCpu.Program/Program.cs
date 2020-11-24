02cf6e3232752e01be4cba3cd26877f0f00195b7
Aaron Stannard <aaron@petabridge.com>
2020-11-24 17:27:28 -0600

    adding idle CPU measurement benchmark using Docker

---
+++ src/tests/Helios.DedicatedThreadPool.IdleCpu.Program/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Helios.Concurrency;
+
+namespace Helios.DedicatedThreadPool.IdleCpu.Program
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var maxThreads = Math.Max(4, Environment.ProcessorCount);
+            
+            Console.WriteLine("Starting Idle Cpu Test with Following Configuration");
+            Console.WriteLine("MaxThreads: {0}", maxThreads);
+
+            var settings = new DedicatedThreadPoolSettings(maxThreads);
+            var threadPool = new Concurrency.DedicatedThreadPool(settings);
+
+            await threadPool.WaitForThreadsExit();
+        }
+    }
+}
